350c3d60a811064e072ea4412e1284151e3e4e91
kuleic <andrijana.kulezic@gmail.com>
2020-12-29 13:40:26 +0100

    Initial commit

---
+++ src/main/java/com/a2/zuulapiservice/ZuulApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.a2.zuulapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/a2/zuulapiservice/ZuulApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a2.zuulapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
